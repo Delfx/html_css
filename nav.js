1d81fa16474d8e853fc5e79b1b9a97d66f068dc1
Lukas Motiejūnas <motiejunas.lukas@gmail.com>
2018-10-16 23:14:07 +0300

    Initial commit

---
+++ nav.js
@@ -0,0 +1,8 @@
+document.write('\
+<div class="nav">\
+    <ul id="navbar">\
+        <li class="navli"><a href="index.html">Home</a></li>\
+        <li class="navli"><a href="about.html">About</a></li>\
+    </ul>\
+</div>\
+');
